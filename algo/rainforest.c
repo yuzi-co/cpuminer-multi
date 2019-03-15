0c238aad1129c0f8f12f7a93467e6e77459f8380
yuzi-co <vadim@yuzi.co>
2019-03-15 20:21:20 +0200

    use uint64_t

--- algo/rainforest.c
+++ algo/rainforest.c
@@ -561,7 +561,7 @@ static inline void rf_w128(uint64_t *cell, uint64_t ofs, uint64_t x, uint64_t y)
   // used between two calls to keep the pipe full.
   asm volatile("stp %0, %1, [%2,#%3]\n\t"
                : /* no output */
-               : "r"(x), "r"(y), "r" (cell), "I" (ofs*8));
+               : "r"(x), "r"(y), "r" (cell), "l" (ofs*8));
 #else
   cell[ofs+0] = x;
   cell[ofs+1] = y;
